074f2581eade3427b9ec12fbba62ccf7c385eb91
Johan Ospina <johanos@bu.edu>
2014-12-16 13:45:23 -0500

    nope

--- Magis-Xcode-project/Magis/FourthViewController.swift
+++ Magis-Xcode-project/Magis/FourthViewController.swift
@@ -9,14 +9,20 @@
 
 
 //CKCalendar Should be here
-class FourthViewController: UIViewController {
+class FourthViewController: UIViewController, CKCalendarDelegate {
+    
+    let dateFormater : NSDateFormatter = NSDateFormatter()
   
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateFormater.dateFormat = "MM/dd/yyyy"
        
    
         let calendar = CKCalendarView(frame: CGRectMake(0, 60, self.view.bounds.width, self.view.bounds.height));
         self.view.addSubview(calendar);
+        calendar.delegate = self
+        
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -26,10 +32,10 @@ class FourthViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
    
-    @IBAction func goToEventFeedTV(sender: AnyObject) {
+    //@IBAction func goToEventFeedTV(sender: AnyObject) {
         //the storyboard is calling prepareForSegue for us and performSegueWithIdentifier
 
-    }
+   // }
 
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -39,6 +45,16 @@ class FourthViewController: UIViewController {
         
         VC.eventList = eventsListToPass
         VC.eventsToLoad = eventsListToPass.count
+        
+    }
+    
+    func calendar(calendar: CKCalendarView!, didSelectDate date: NSDate!) {
+        println("I chose a Date! its: \(dateFormater.stringFromDate(date!))")
+        
+        var detailsVC : DetailEventVC = DetailEventVC()
+        
+        self.presentViewController(detailsVC, animated: true, completion: {println("completed push") })
+        
         
     }
 }
